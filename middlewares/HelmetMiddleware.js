787486bf8bf238eab9eca75ae92c094616b69f3f
tjwrightnow <122188994+tjwrightnow@users.noreply.github.com>
2026-02-13 17:28:38 +0500

    Update HelmetMiddleware.js

--- middlewares/HelmetMiddleware.js
+++ middlewares/HelmetMiddleware.js
@@ -7,7 +7,7 @@ const SecurityHeaders = helmet({
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'"],
       styleSrc: ["'self'", "'unsafe-inline'"],
-      imgSrc: ["'self'", "data:", "blob:", "http://localhost:5000", "http://localhost:5173", "http://31.97.141.173:5000/api/login","http://swipestate-adminpanel.vercel.app", "https://swipestate-adminpanel.vercel.app", "http://swipestate-adminpanel.vercel.app/", "https://swipestate-adminpanel.vercel.app/"],
+      imgSrc: ["'self'", "data:", "blob:", "http://localhost:3000", "http://localhost:5000", "http://localhost:5173", "http://31.97.141.173:5000/api/login","http://swipestate-adminpanel.vercel.app", "https://swipestate-adminpanel.vercel.app", "http://swipestate-adminpanel.vercel.app/", "https://swipestate-adminpanel.vercel.app/"],
       connectSrc: ["'self'", "http://localhost:3000", "http://localhost:5000", "http://localhost:5173", "http://31.97.141.173:5000/api/login", "http://swipestate-adminpanel.vercel.app", "https://swipestate-adminpanel.vercel.app", "http://swipestate-adminpanel.vercel.app/", "https://swipestate-adminpanel.vercel.app/"],
       fontSrc: ["'self'", "https:", "data:"],
       objectSrc: ["'none'"],
